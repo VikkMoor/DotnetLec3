5fbc3a06d0020bee6f6b1ea075255f91af922907
Vikk_Moor <victimofuroboros@gmail.com>
2022-11-20 15:57:49 +0500

    OnlyOneReady

---
+++ 3rd/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+PrintArray(arr);
+
+
+// void SelectionSort(int[] array)
+// {
+//     for(int i =  0; i<array.Length; i++)
+//     {
+//         int minPosition = 1;
+
+
+//         int temporary = array[i];
+//         array[i] = 
+//     }
+// }
+
