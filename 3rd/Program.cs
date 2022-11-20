e41d7d96fdd8fa538ef0f695865a21ffd5be28ce
Vikk_Moor <victimofuroboros@gmail.com>
2022-11-20 20:49:41 +0500

    3rdReady

--- 3rd/Program.cs
+++ 3rd/Program.cs
@@ -7,22 +7,29 @@ void PrintArray(int[] array)
 
     for (int i = 0; i < count; i++)
     {
-        Console.WriteLine(i);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
     }
 }
 
 PrintArray(arr);
+SelectionSort(arr);
 
-
-// void SelectionSort(int[] array)
-// {
-//     for(int i =  0; i<array.Length; i++)
-//     {
-//         int minPosition = 1;
-
-
-//         int temporary = array[i];
-//         array[i] = 
-//     }
-// }
-
+PrintArray(arr);
